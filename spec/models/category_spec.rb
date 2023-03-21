6d358ea2fb323b3e5f15abd9ddc45797931b188c
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-21 14:30:08 +0500

    test(category_spec.rb): add association and validation tests for Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#Association tests' do
+    it { should belong_to(:user) }
+    it { should have_and_belong_to_many(:expenses) }
+  end
+
+  describe '#Validations tests' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(50) }
+    it { should validate_presence_of(:icon) }
+  end
 end
