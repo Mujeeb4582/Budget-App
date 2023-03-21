aead27a8c177b12a21e181bf445661fed6cc98a4
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-21 16:23:44 +0500

    chore(expense.rb): fix typo in validates method call for name attribute test(expense_spec.rb): add tests for validations and associations of Expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,6 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
 
-  validates name, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0, message: 'Amount must be positive!' }
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#Associated tests' do
+    it { should belong_to(:user).without_validating_presence }
+    it { should have_and_belong_to_many(:categories) }
+  end
+
+  describe '#Validations tests' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(50) }
+    it { should validate_presence_of(:amount) }
+    it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
+  end
 end
