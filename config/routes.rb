46b3abb7c520ffad908767d2fa465fb46cf8a582
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-22 22:54:17 +0500

    feat(routes.rb): add categories resource with index, show, new, and create actions refactor(routes.rb): restrict users resource to only index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
   devise_for :users
-  resources 'users'
+  resources :users, only: [:index]
+  resources :categories, only: [:index, :show, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
