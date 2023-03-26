99eb63dc23134056f0a4744e6966c1e17da879b3
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-26 23:40:29 +0500

    remove the bug

--- db/migrate/20230326183430_create_expenses.rb
+++ db/migrate/20230326183430_create_expenses.rb
@@ -1,10 +1,9 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
-      create_table :expenses do |t|
-        t.string :name
-        t.decimal :amount
-        t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.decimal :amount
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
