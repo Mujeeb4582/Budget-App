abcb30780358825786666142aac31a369e8de71d
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-04-16 23:18:54 +0500

    Add method to update the sign_up field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
+  protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  # end
-
-  before_action :update_allowed_parameters, if: :devise_controller?
-
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :avatar)
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
   end
 
 end
